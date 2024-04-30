97fef2955494c77bf58d2b99707a586a0a5ea8f5
YoussefEltelwany <168205451+YoussefEltelwany@users.noreply.github.com>
2024-04-30 22:20:22 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,18 +1,27 @@
-let Number1 = parseInt(document.getElementById("frist").value);
+
+function Minus (Number1, Number2){
+  let Number1 = parseInt(document.getElementById("frist").value);
 
   let Number2 = parseInt(document.getElementById("last").value);
 
-function Minus (Number1, Number2){
    var MinusResult =Number1 -Number2 ;   
    document.getElementById("result").value =MinusResult ;
 }
 
 function sum (Number1, Number2){
+  let Number1 = parseInt(document.getElementById("frist").value);
+
+  let Number2 = parseInt(document.getElementById("last").value);
+
    var sumResult =Number1 + Number2 ;
    document.getElementById("result").value =sumResult ;
 }
 
 function ksma (Number1, Number2){
+  let Number1 = parseInt(document.getElementById("frist").value);
+
+  let Number2 = parseInt(document.getElementById("last").value);
+
       var ksmaResult =Number1 / Number2 ;  
       if(Number2 == 0 ){
          ksmaResult= ("can't divid by 0");
@@ -21,6 +30,10 @@ function ksma (Number1, Number2){
    }
 
 function darb (Number1, Number2){
+  let Number1 = parseInt(document.getElementById("frist").value);
+
+  let Number2 = parseInt(document.getElementById("last").value);
+
       var DarbResult =Number1 * Number2 ;
       document.getElementById("result").value =DarbResult ;
    
